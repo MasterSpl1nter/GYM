57b42662ecbae877d221e0cf11a69bc3f5683284
matanr1 <matanr1@gmail.com>
2014-04-27 12:57:29 +0300

    add singelton CALSS

---
+++ newGym/newGym/SingleUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace newGym
+{
+    class SingleUser
+    {
+        private static Lazy<SingleUser> instance = new Lazy<SingleUser>(() => new SingleUser());
+
+        private SingleUser() { }
+
+        private Person person;
+
+        public static SingleUser Instance
+        {
+            get
+            {
+                if (instance.IsValueCreated == false)
+                {
+                    instance = new Lazy<SingleUser>(() => new SingleUser());
+                }
+                return instance.Value;
+            }
+        }
+
+        public void set_user(Person person)
+        {
+            this.person = person;
+        }
+
+
+        public Person get_user()
+        {
+            return this.person;
+        }
+    }
+}
