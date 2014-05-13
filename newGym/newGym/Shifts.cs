2a06cd1ab05e1620cae33534c4ca5cb572ad496b
MasterSpl1nter <guyge@ac.sce.ac.il>
2014-05-14 01:07:32 +0300

    Added Guide Shifts file, not working yet.

---
+++ newGym/newGym/Shifts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace newGym
+{
+    public partial class Shifts : Form
+    {
+        //not working yet.
+        DataTable dt;
+        public Shifts()
+        {
+            InitializeComponent();
+            MySQL.Query(dt, "SELECT class.id,class.name,class.room,classtime.starttime,classtime.endtime FROM class INNER JOIN classtime ON class.id=classtime.classid WHERE class.guideid=" + SingleUser.Instance.get_user().Id);
+            dataGridView1.DataSource = dt;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+        }
+        private void dataGridView1_CellClick(object sender,DataGridViewCellEventArgs e)
+        {
+                MessageBox.Show(dt.Rows[e.RowIndex]["id"].ToString());
+
+        }
+    }
+}
