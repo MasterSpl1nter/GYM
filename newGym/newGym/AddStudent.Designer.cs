644f67a7bb2bc7db7814d758c95c392e791f319e
yosi352 <yos35216@gmail.com>
2014-05-13 19:40:13 +0300

    student pannels

--- newGym/newGym/AddStudent.Designer.cs
+++ newGym/newGym/AddStudent.Designer.cs
@@ -56,13 +56,13 @@
             // 
             // id_box
             // 
-            this.id_box.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.id_box.Location = new System.Drawing.Point(151, 39);
+            this.id_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.id_box.Location = new System.Drawing.Point(151, 33);
             this.id_box.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.id_box.MaxLength = 9;
             this.id_box.Multiline = true;
             this.id_box.Name = "id_box";
-            this.id_box.Size = new System.Drawing.Size(100, 20);
+            this.id_box.Size = new System.Drawing.Size(145, 26);
             this.id_box.TabIndex = 1;
             this.id_box.TextChanged += new System.EventHandler(this.id_box_TextChanged);
             this.id_box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.id_box_KeyPress);
@@ -71,9 +71,9 @@
             // 
             this.id_label.AutoSize = true;
             this.id_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.id_label.Location = new System.Drawing.Point(122, 45);
+            this.id_label.Location = new System.Drawing.Point(44, 41);
             this.id_label.Name = "id_label";
-            this.id_label.Size = new System.Drawing.Size(23, 14);
+            this.id_label.Size = new System.Drawing.Size(29, 18);
             this.id_label.TabIndex = 1;
             this.id_label.Text = "ID:";
             // 
@@ -81,9 +81,9 @@
             // 
             this.firstName_label.AutoSize = true;
             this.firstName_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.firstName_label.Location = new System.Drawing.Point(77, 70);
+            this.firstName_label.Location = new System.Drawing.Point(44, 70);
             this.firstName_label.Name = "firstName_label";
-            this.firstName_label.Size = new System.Drawing.Size(68, 14);
+            this.firstName_label.Size = new System.Drawing.Size(84, 18);
             this.firstName_label.TabIndex = 3;
             this.firstName_label.Text = "First Name:";
             // 
@@ -91,9 +91,9 @@
             // 
             this.lastName_label.AutoSize = true;
             this.lastName_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lastName_label.Location = new System.Drawing.Point(78, 98);
+            this.lastName_label.Location = new System.Drawing.Point(44, 98);
             this.lastName_label.Name = "lastName_label";
-            this.lastName_label.Size = new System.Drawing.Size(67, 14);
+            this.lastName_label.Size = new System.Drawing.Size(82, 18);
             this.lastName_label.TabIndex = 5;
             this.lastName_label.Text = "Last name:";
             // 
@@ -103,7 +103,7 @@
             this.birth_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.birth_label.Location = new System.Drawing.Point(75, 249);
             this.birth_label.Name = "birth_label";
-            this.birth_label.Size = new System.Drawing.Size(65, 14);
+            this.birth_label.Size = new System.Drawing.Size(76, 18);
             this.birth_label.TabIndex = 8;
             this.birth_label.Text = "Birth date:";
             // 
@@ -113,7 +113,7 @@
             this.start_date_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.start_date_label.Location = new System.Drawing.Point(55, 281);
             this.start_date_label.Name = "start_date_label";
-            this.start_date_label.Size = new System.Drawing.Size(89, 14);
+            this.start_date_label.Size = new System.Drawing.Size(104, 18);
             this.start_date_label.TabIndex = 9;
             this.start_date_label.Text = "Start Contract:";
             // 
@@ -124,7 +124,7 @@
             this.end_date_label.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.end_date_label.Location = new System.Drawing.Point(62, 309);
             this.end_date_label.Name = "end_date_label";
-            this.end_date_label.Size = new System.Drawing.Size(82, 14);
+            this.end_date_label.Size = new System.Drawing.Size(95, 18);
             this.end_date_label.TabIndex = 10;
             this.end_date_label.Text = "End contract:";
             // 
@@ -134,7 +134,7 @@
             this.medcart_lable.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.medcart_lable.Location = new System.Drawing.Point(9, 344);
             this.medcart_lable.Name = "medcart_lable";
-            this.medcart_lable.Size = new System.Drawing.Size(136, 14);
+            this.medcart_lable.Size = new System.Drawing.Size(161, 18);
             this.medcart_lable.TabIndex = 14;
             this.medcart_lable.Text = "valid medical certificate:";
             // 
@@ -142,7 +142,7 @@
             // 
             this.enter_button.BackColor = System.Drawing.SystemColors.Highlight;
             this.enter_button.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.enter_button.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter_button.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.enter_button.ForeColor = System.Drawing.Color.Navy;
             this.enter_button.Location = new System.Drawing.Point(118, 382);
             this.enter_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -157,16 +157,16 @@
             // 
             this.mail_label.AutoSize = true;
             this.mail_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mail_label.Location = new System.Drawing.Point(106, 126);
+            this.mail_label.Location = new System.Drawing.Point(44, 126);
             this.mail_label.Name = "mail_label";
-            this.mail_label.Size = new System.Drawing.Size(38, 14);
+            this.mail_label.Size = new System.Drawing.Size(46, 18);
             this.mail_label.TabIndex = 18;
             this.mail_label.Text = "Email:";
             // 
             // birth_date
             // 
             this.birth_date.CalendarFont = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.birth_date.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.birth_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.birth_date.Location = new System.Drawing.Point(150, 243);
             this.birth_date.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.birth_date.Name = "birth_date";
@@ -176,7 +176,7 @@
             // end_date
             // 
             this.end_date.CalendarFont = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.end_date.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.end_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.end_date.Location = new System.Drawing.Point(151, 301);
             this.end_date.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.end_date.Name = "end_date";
@@ -186,7 +186,7 @@
             // med_cart_date
             // 
             this.med_cart_date.CalendarFont = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.med_cart_date.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.med_cart_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.med_cart_date.Location = new System.Drawing.Point(150, 338);
             this.med_cart_date.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.med_cart_date.Name = "med_cart_date";
@@ -196,7 +196,7 @@
             // start_date
             // 
             this.start_date.CalendarFont = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.start_date.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.start_date.Location = new System.Drawing.Point(151, 275);
             this.start_date.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.start_date.Name = "start_date";
@@ -205,59 +205,59 @@
             // 
             // firstname_box
             // 
-            this.firstname_box.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstname_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.firstname_box.Location = new System.Drawing.Point(151, 67);
             this.firstname_box.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.firstname_box.Multiline = true;
             this.firstname_box.Name = "firstname_box";
-            this.firstname_box.Size = new System.Drawing.Size(100, 20);
+            this.firstname_box.Size = new System.Drawing.Size(145, 20);
             this.firstname_box.TabIndex = 2;
             // 
             // lastname_box
             // 
-            this.lastname_box.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastname_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lastname_box.Location = new System.Drawing.Point(151, 95);
             this.lastname_box.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.lastname_box.Multiline = true;
             this.lastname_box.Name = "lastname_box";
-            this.lastname_box.Size = new System.Drawing.Size(100, 20);
+            this.lastname_box.Size = new System.Drawing.Size(145, 20);
             this.lastname_box.TabIndex = 3;
             // 
             // email_box
             // 
-            this.email_box.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.email_box.Location = new System.Drawing.Point(151, 123);
             this.email_box.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.email_box.Multiline = true;
             this.email_box.Name = "email_box";
-            this.email_box.Size = new System.Drawing.Size(100, 20);
+            this.email_box.Size = new System.Drawing.Size(145, 20);
             this.email_box.TabIndex = 4;
             // 
             // tb_repeatPass
             // 
-            this.tb_repeatPass.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_repeatPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tb_repeatPass.Location = new System.Drawing.Point(151, 206);
             this.tb_repeatPass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tb_repeatPass.Name = "tb_repeatPass";
-            this.tb_repeatPass.Size = new System.Drawing.Size(100, 22);
+            this.tb_repeatPass.Size = new System.Drawing.Size(145, 22);
             this.tb_repeatPass.TabIndex = 11;
             // 
             // tb_pass
             // 
-            this.tb_pass.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tb_pass.Location = new System.Drawing.Point(151, 175);
             this.tb_pass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tb_pass.Name = "tb_pass";
-            this.tb_pass.Size = new System.Drawing.Size(100, 22);
+            this.tb_pass.Size = new System.Drawing.Size(145, 22);
             this.tb_pass.TabIndex = 10;
             // 
             // tb_username
             // 
-            this.tb_username.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tb_username.Location = new System.Drawing.Point(150, 149);
             this.tb_username.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tb_username.Name = "tb_username";
-            this.tb_username.Size = new System.Drawing.Size(100, 22);
+            this.tb_username.Size = new System.Drawing.Size(146, 22);
             this.tb_username.TabIndex = 9;
             // 
             // verPassWorod_label
@@ -265,9 +265,9 @@
             this.verPassWorod_label.AutoSize = true;
             this.verPassWorod_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.verPassWorod_label.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.verPassWorod_label.Location = new System.Drawing.Point(59, 209);
+            this.verPassWorod_label.Location = new System.Drawing.Point(44, 214);
             this.verPassWorod_label.Name = "verPassWorod_label";
-            this.verPassWorod_label.Size = new System.Drawing.Size(85, 14);
+            this.verPassWorod_label.Size = new System.Drawing.Size(101, 18);
             this.verPassWorod_label.TabIndex = 26;
             this.verPassWorod_label.Text = "Ver password:";
             // 
@@ -276,9 +276,9 @@
             this.passWord_label.AutoSize = true;
             this.passWord_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.passWord_label.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.passWord_label.Location = new System.Drawing.Point(83, 178);
+            this.passWord_label.Location = new System.Drawing.Point(44, 183);
             this.passWord_label.Name = "passWord_label";
-            this.passWord_label.Size = new System.Drawing.Size(62, 14);
+            this.passWord_label.Size = new System.Drawing.Size(74, 18);
             this.passWord_label.TabIndex = 25;
             this.passWord_label.Text = "Password:";
             // 
@@ -286,9 +286,9 @@
             // 
             this.userName_label.AutoSize = true;
             this.userName_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userName_label.Location = new System.Drawing.Point(75, 152);
+            this.userName_label.Location = new System.Drawing.Point(43, 157);
             this.userName_label.Name = "userName_label";
-            this.userName_label.Size = new System.Drawing.Size(69, 14);
+            this.userName_label.Size = new System.Drawing.Size(85, 18);
             this.userName_label.TabIndex = 24;
             this.userName_label.Text = "User name:";
             // 
@@ -296,7 +296,7 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Highlight;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Navy;
             this.button1.Location = new System.Drawing.Point(206, 382);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -309,7 +309,7 @@
             // 
             // AddStudent
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Menu;
             this.ClientSize = new System.Drawing.Size(339, 440);
