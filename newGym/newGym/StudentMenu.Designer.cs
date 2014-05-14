339c092b26032da4274f3f7fb84a8d6733deb5c5
NiNiCkNaMe <yashaev@gmail.com>
2014-05-14 23:36:22 +0300

    Revert "student pannels"
    This reverts commit 644f67a7bb2bc7db7814d758c95c392e791f319e.

--- newGym/newGym/StudentMenu.Designer.cs
+++ newGym/newGym/StudentMenu.Designer.cs
@@ -34,29 +34,12 @@
             this.label1 = new System.Windows.Forms.Label();
             this.student_name = new System.Windows.Forms.Label();
             this.Cancel_course = new System.Windows.Forms.Button();
-            this.edit_d_panel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.tb_username = new System.Windows.Forms.TextBox();
-            this.userName_label = new System.Windows.Forms.Label();
-            this.start_date = new System.Windows.Forms.DateTimePicker();
-            this.med_cart_date = new System.Windows.Forms.DateTimePicker();
-            this.end_date = new System.Windows.Forms.DateTimePicker();
-            this.birth_date = new System.Windows.Forms.DateTimePicker();
-            this.mail_label = new System.Windows.Forms.Label();
-            this.enter_button = new System.Windows.Forms.Button();
-            this.medcart_lable = new System.Windows.Forms.Label();
-            this.end_date_label = new System.Windows.Forms.Label();
-            this.start_date_label = new System.Windows.Forms.Label();
-            this.birth_label = new System.Windows.Forms.Label();
-            this.lastName_label = new System.Windows.Forms.Label();
-            this.firstName_label = new System.Windows.Forms.Label();
-            this.email_box = new System.Windows.Forms.TextBox();
-            this.lastname_box = new System.Windows.Forms.TextBox();
-            this.firstname_box = new System.Windows.Forms.TextBox();
-            this.edit_d_panel.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
@@ -85,7 +68,6 @@
             this.add_course.TabIndex = 2;
             this.add_course.Text = "Add course";
             this.add_course.UseVisualStyleBackColor = true;
-            this.add_course.Click += new System.EventHandler(this.add_course_Click);
             // 
             // label1
             // 
@@ -113,46 +95,36 @@
             this.Cancel_course.Text = "Cancel course";
             this.Cancel_course.UseVisualStyleBackColor = true;
             // 
-            // edit_d_panel
+            // panel1
             // 
-            this.edit_d_panel.Controls.Add(this.button1);
-            this.edit_d_panel.Location = new System.Drawing.Point(3, 3);
-            this.edit_d_panel.Name = "edit_d_panel";
-            this.edit_d_panel.Size = new System.Drawing.Size(332, 430);
-            this.edit_d_panel.TabIndex = 6;
-            this.edit_d_panel.Visible = false;
-            this.edit_d_panel.Paint += new System.Windows.Forms.PaintEventHandler(this.edit_d_panel_Paint);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(319, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(436, 516);
+            this.panel1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.panel3);
-            this.panel2.Location = new System.Drawing.Point(362, 28);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(393, 513);
             this.panel2.TabIndex = 1;
             // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.tb_username);
-            this.panel3.Controls.Add(this.userName_label);
-            this.panel3.Controls.Add(this.edit_d_panel);
-            this.panel3.Controls.Add(this.start_date);
-            this.panel3.Controls.Add(this.med_cart_date);
-            this.panel3.Controls.Add(this.end_date);
-            this.panel3.Controls.Add(this.birth_date);
-            this.panel3.Controls.Add(this.mail_label);
-            this.panel3.Controls.Add(this.enter_button);
-            this.panel3.Controls.Add(this.medcart_lable);
-            this.panel3.Controls.Add(this.end_date_label);
-            this.panel3.Controls.Add(this.start_date_label);
-            this.panel3.Controls.Add(this.birth_label);
-            this.panel3.Controls.Add(this.lastName_label);
-            this.panel3.Controls.Add(this.firstName_label);
-            this.panel3.Controls.Add(this.email_box);
-            this.panel3.Controls.Add(this.lastname_box);
-            this.panel3.Controls.Add(this.firstname_box);
             this.panel3.Controls.Add(this.textBox1);
-            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Location = new System.Drawing.Point(3, 0);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(332, 516);
             this.panel3.TabIndex = 0;
@@ -166,196 +138,13 @@
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(91, 114);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // tb_username
-            // 
-            this.tb_username.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_username.Location = new System.Drawing.Point(142, 185);
-            this.tb_username.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tb_username.Name = "tb_username";
-            this.tb_username.Size = new System.Drawing.Size(178, 26);
-            this.tb_username.TabIndex = 58;
-            // 
-            // userName_label
-            // 
-            this.userName_label.AutoSize = true;
-            this.userName_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userName_label.Location = new System.Drawing.Point(54, 185);
-            this.userName_label.Name = "userName_label";
-            this.userName_label.Size = new System.Drawing.Size(85, 18);
-            this.userName_label.TabIndex = 64;
-            this.userName_label.Text = "User name:";
-            // 
-            // start_date
-            // 
-            this.start_date.CalendarFont = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.start_date.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.start_date.Location = new System.Drawing.Point(142, 277);
-            this.start_date.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.start_date.Name = "start_date";
-            this.start_date.Size = new System.Drawing.Size(219, 26);
-            this.start_date.TabIndex = 54;
-            // 
-            // med_cart_date
-            // 
-            this.med_cart_date.CalendarFont = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.med_cart_date.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.med_cart_date.Location = new System.Drawing.Point(144, 364);
-            this.med_cart_date.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.med_cart_date.Name = "med_cart_date";
-            this.med_cart_date.Size = new System.Drawing.Size(217, 26);
-            this.med_cart_date.TabIndex = 56;
-            // 
-            // end_date
-            // 
-            this.end_date.CalendarFont = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.end_date.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.end_date.Location = new System.Drawing.Point(144, 318);
-            this.end_date.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.end_date.Name = "end_date";
-            this.end_date.Size = new System.Drawing.Size(217, 26);
-            this.end_date.TabIndex = 55;
-            // 
-            // birth_date
-            // 
-            this.birth_date.CalendarFont = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.birth_date.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.birth_date.Location = new System.Drawing.Point(144, 237);
-            this.birth_date.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.birth_date.Name = "birth_date";
-            this.birth_date.Size = new System.Drawing.Size(217, 26);
-            this.birth_date.TabIndex = 52;
-            // 
-            // mail_label
-            // 
-            this.mail_label.AutoSize = true;
-            this.mail_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mail_label.Location = new System.Drawing.Point(85, 143);
-            this.mail_label.Name = "mail_label";
-            this.mail_label.Size = new System.Drawing.Size(46, 18);
-            this.mail_label.TabIndex = 63;
-            this.mail_label.Text = "Email:";
-            // 
-            // enter_button
-            // 
-            this.enter_button.BackColor = System.Drawing.SystemColors.Highlight;
-            this.enter_button.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.enter_button.ForeColor = System.Drawing.Color.Navy;
-            this.enter_button.Location = new System.Drawing.Point(166, 433);
-            this.enter_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.enter_button.Name = "enter_button";
-            this.enter_button.Size = new System.Drawing.Size(154, 33);
-            this.enter_button.TabIndex = 61;
-            this.enter_button.Text = "edit";
-            this.enter_button.UseVisualStyleBackColor = false;
-            this.enter_button.Click += new System.EventHandler(this.enter_button_Click);
-            // 
-            // medcart_lable
-            // 
-            this.medcart_lable.AutoSize = true;
-            this.medcart_lable.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.medcart_lable.Location = new System.Drawing.Point(-28, 370);
-            this.medcart_lable.Name = "medcart_lable";
-            this.medcart_lable.Size = new System.Drawing.Size(161, 18);
-            this.medcart_lable.TabIndex = 62;
-            this.medcart_lable.Text = "valid medical certificate:";
-            // 
-            // end_date_label
-            // 
-            this.end_date_label.AutoSize = true;
-            this.end_date_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.end_date_label.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.end_date_label.Location = new System.Drawing.Point(38, 324);
-            this.end_date_label.Name = "end_date_label";
-            this.end_date_label.Size = new System.Drawing.Size(95, 18);
-            this.end_date_label.TabIndex = 60;
-            this.end_date_label.Text = "End contract:";
-            // 
-            // start_date_label
-            // 
-            this.start_date_label.AutoSize = true;
-            this.start_date_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.start_date_label.Location = new System.Drawing.Point(29, 277);
-            this.start_date_label.Name = "start_date_label";
-            this.start_date_label.Size = new System.Drawing.Size(104, 18);
-            this.start_date_label.TabIndex = 59;
-            this.start_date_label.Text = "Start Contract:";
-            // 
-            // birth_label
-            // 
-            this.birth_label.AutoSize = true;
-            this.birth_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.birth_label.Location = new System.Drawing.Point(57, 237);
-            this.birth_label.Name = "birth_label";
-            this.birth_label.Size = new System.Drawing.Size(76, 18);
-            this.birth_label.TabIndex = 57;
-            this.birth_label.Text = "Birth date:";
-            // 
-            // lastName_label
-            // 
-            this.lastName_label.AutoSize = true;
-            this.lastName_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lastName_label.Location = new System.Drawing.Point(51, 96);
-            this.lastName_label.Name = "lastName_label";
-            this.lastName_label.Size = new System.Drawing.Size(82, 18);
-            this.lastName_label.TabIndex = 53;
-            this.lastName_label.Text = "Last name:";
-            // 
-            // firstName_label
-            // 
-            this.firstName_label.AutoSize = true;
-            this.firstName_label.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.firstName_label.Location = new System.Drawing.Point(49, 60);
-            this.firstName_label.Name = "firstName_label";
-            this.firstName_label.Size = new System.Drawing.Size(84, 18);
-            this.firstName_label.TabIndex = 50;
-            this.firstName_label.Text = "First Name:";
-            // 
-            // email_box
-            // 
-            this.email_box.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.email_box.Location = new System.Drawing.Point(144, 140);
-            this.email_box.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.email_box.Multiline = true;
-            this.email_box.Name = "email_box";
-            this.email_box.Size = new System.Drawing.Size(176, 28);
-            this.email_box.TabIndex = 51;
-            // 
-            // lastname_box
-            // 
-            this.lastname_box.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lastname_box.Location = new System.Drawing.Point(144, 86);
-            this.lastname_box.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.lastname_box.Multiline = true;
-            this.lastname_box.Name = "lastname_box";
-            this.lastname_box.Size = new System.Drawing.Size(176, 28);
-            this.lastname_box.TabIndex = 49;
-            // 
-            // firstname_box
-            // 
-            this.firstname_box.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.firstname_box.Location = new System.Drawing.Point(144, 51);
-            this.firstname_box.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.firstname_box.Multiline = true;
-            this.firstname_box.Name = "firstname_box";
-            this.firstname_box.Size = new System.Drawing.Size(176, 27);
-            this.firstname_box.TabIndex = 48;
-            // 
             // StudentMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(767, 540);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.Cancel_course);
             this.Controls.Add(this.student_name);
             this.Controls.Add(this.label1);
@@ -364,7 +153,7 @@
             this.Name = "StudentMenu";
             this.Text = "StudentMenu";
             this.Load += new System.EventHandler(this.StudentMenu_Load);
-            this.edit_d_panel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
@@ -381,27 +170,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label student_name;
         private System.Windows.Forms.Button Cancel_course;
-        private System.Windows.Forms.Panel edit_d_panel;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox tb_username;
-        private System.Windows.Forms.Label userName_label;
-        private System.Windows.Forms.DateTimePicker start_date;
-        private System.Windows.Forms.DateTimePicker med_cart_date;
-        private System.Windows.Forms.DateTimePicker end_date;
-        private System.Windows.Forms.DateTimePicker birth_date;
-        private System.Windows.Forms.Label mail_label;
-        private System.Windows.Forms.Button enter_button;
-        private System.Windows.Forms.Label medcart_lable;
-        private System.Windows.Forms.Label end_date_label;
-        private System.Windows.Forms.Label start_date_label;
-        private System.Windows.Forms.Label birth_label;
-        private System.Windows.Forms.Label lastName_label;
-        private System.Windows.Forms.Label firstName_label;
-        private System.Windows.Forms.TextBox email_box;
-        private System.Windows.Forms.TextBox lastname_box;
-        private System.Windows.Forms.TextBox firstname_box;
     }
 }
--- newGym/newGym/StudentMenu.cs
+++ newGym/newGym/StudentMenu.cs
@@ -7,22 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
-using System.Text.RegularExpressions;
 
 namespace newGym
 {
     public partial class StudentMenu : Form
     {
-         Student student;
-        int id;
-        string firstName;
-        string LastName;
-        string email; // add
-        string username; // get from this form
-        string password; // get from this form
-        int permission = 0;
-        DateTime now = DateTime.Now;
+
         public StudentMenu()
         {
             InitializeComponent();
@@ -50,148 +40,8 @@ namespace newGym
 
         private void edit_button_Click(object sender, EventArgs e)
         {
-            edit_d_panel.Visible = true;
-        }
-
-        private void edit_d_panel_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void enter_button_Click(object sender, EventArgs e)
-        {
-                DateTime startDate = DateTime.Now;
-                resetColors();
-
-                try
-                {
-
-
-
-                    //check name of the student
-                    if (firstname_box.Text == "")
-                    {
-                        MessageBox.Show("Please enter First name");
-                        firstName_label.ForeColor = Color.Red;
-                        return;
-                    }
-
-                    //check lastname of the student
-                    if (lastname_box.Text == "")
-                    {
-                        MessageBox.Show(" Please enter Last name!");
-                        lastName_label.ForeColor = Color.Red;
-                        return;
-                    }
-                    //check email valitiy
-                    if (email_box.Text != "")
-                    {
-                        Regex r = new Regex(@"[\w-]+@([\w-]+\.)+[\w-]+");
-                        if (!r.IsMatch(email_box.Text))
-                        {
-                            MessageBox.Show("Email invalid , Press correct email ", "ERROR", MessageBoxButtons.OK);
-                            email_box.ResetText();
-                            email_box.Focus();
-                            mail_label.ForeColor = Color.Red;
-                            return;
-                        }
-                    }
-                    // check username
-                    if (tb_username.Text == "")
-                    {
-                        MessageBox.Show("Please enter user name ");
-                        return;
-                    }
-                    //check validaty of dates
-                    if (now.Date <= birth_date.Value.Date)
-                    {
-                        MessageBox.Show("Birth date invalid , please try again!");
-                        birth_label.ForeColor = Color.Red;
-                        birth_date.Focus();
-                        return;
-                    }
-
-                    /*if (startDate.Date > start_date.Value.Date)
-                    {
-                        MessageBox.Show("Start date is invalid , please try again!");
-                        start_date_label.ForeColor = Color.Red;
-                        start_date.Focus();
-                        return;
-                    }*/
-                    /* if (start_date.Value.Date >= end_date.Value.Date)
-                     {
-                         MessageBox.Show("End date is invalid , please try again");
-                         end_date_label.ForeColor = Color.Red;
-                         end_date.Focus();
-                         return;
-                     }*/
-                    if (med_cart_date.Value.Date > start_date.Value.Date && med_cart_date.Value.Date < end_date.Value.Date)
-                    {
-                        MessageBox.Show(" Medical certificate is valid but has to update the certificate for the duration of the subscription ");
-                    }
-                    if (med_cart_date.Value.Date < start_date.Value.Date)
-                    {
-                        MessageBox.Show(" Medical certificate is not valid medical certificate must be brought immediate");
-                    }
-
-
-
-
-                    if (firstname_box.Text != "" && lastname_box.Text != "" && birth_date.Value.Date.ToString("yyyy-MM-dd") != ""
-                        && start_date.Value.Date.ToString("yyyy-MM-dd") != "" && end_date.Value.Date.ToString("yyyy-MM-dd") != ""
-                        && med_cart_date.Value.Date.ToString("yyyy-MM-dd") != "" && email_box.Text != "")
-                    {
-                        this.id = SingleUser.Instance.get_user().Id;//Convert.ToInt32(id_box.Text);
-                        this.firstName = firstname_box.Text;
-                        this.LastName = lastname_box.Text;
-                        this.email = email_box.Text;
-                        this.username = tb_username.Text;
-                        string insert = String.Format("{0},'{1}','{2}','{3}','{4}','{5}',{6},'{7}','{8}','{9}','{10}'", id, firstName,
-                            LastName, email, username, password, permission, birth_date.Value.Date.ToString("yyyy-MM-dd"),
-                            start_date.Value.Date.ToString("yyyy-MM-dd"), end_date.Value.Date.ToString("yyyy-MM-dd"),
-                            med_cart_date.Value.Date.ToString("yyyy-MM-dd"));
-                        Student.Delete(id.ToString());
-                        MySQL.Insert("student", "id,firstname,lastname,email,username,password,permission,birthday,startdate,enddate,medcert", insert);
-                        MessageBox.Show("Details have been update succesfully ");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Incomplete fields Please fill them before pressing the button,");
-                        return;
-                    }
-                }
-                catch (MySqlException ex)
-                {
-                    MessageBox.Show("Error no: " + ex.Message);
-                }
-
+            EditStudentBySecrtary ed = new EditStudentBySecrtary();
             
         }
-        private void resetColors()
-        {
-            // zreo all the label's colors
-            firstName_label.ForeColor = Color.Black;
-            lastName_label.ForeColor = Color.Black;
-            mail_label.ForeColor = Color.Black;
-            userName_label.ForeColor = Color.Black;
-            birth_label.ForeColor = Color.Black;
-            start_date_label.ForeColor = Color.Black;
-            end_date_label.ForeColor = Color.Black;
-            medcart_lable.ForeColor = Color.Black;
-
-        }
-        
-
-        private void id_box_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            const char Delete = (char)8;
-            e.Handled = !char.IsDigit(e.KeyChar) && e.KeyChar != Delete;
-        }
-
-        private void add_course_Click(object sender, EventArgs e)
-        {
-        
-        }
-        }
     }
-
+}
